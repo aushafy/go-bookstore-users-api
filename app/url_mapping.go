da5506b75151b6b8df363b3cf4d1643b948d3796
aushafy <aushafy@gmail.com>
2021-04-19 13:31:05 +0700

    refactor the controllers directory structure

--- app/url_mapping.go
+++ app/url_mapping.go
@@ -1,7 +1,13 @@
 package app
 
-import "github.com/aushafy/go-bookstore-users-api/controllers"
+import (
+	"github.com/aushafy/go-bookstore-users-api/controllers/ping"
+	"github.com/aushafy/go-bookstore-users-api/controllers/users"
+)
 
 func mapUrls() {
-	router.GET("/ping", controllers.Ping)
+	router.GET("/ping", ping.Ping)
+	router.GET("/users/:user_id", users.GetUser)
+	// router.GET("/users/search", controllers.SearchUser)
+	router.POST("/users", users.CreateUser)
 }
