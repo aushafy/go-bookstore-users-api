915b68b6674f63746dce07f8060b1fd6ffb175ee
aushafy <aushafy@gmail.com>
2021-04-20 13:32:29 +0700

    user controller api

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,13 +1,37 @@
 package users
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
+	"github.com/aushafy/go-bookstore-users-api/domain/users"
+	"github.com/aushafy/go-bookstore-users-api/services"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	log = logrus.New()
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "Implement me!")
+	var user users.User
+	// catch all user HTTP body request
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		log.Error(err)
+	}
+	// then unmarshal JSON body request to the User Struct
+	if err := json.Unmarshal(bytes, &user); err != nil {
+		log.Error(err)
+	}
+	result, err := services.CreateUser(user)
+	if err != nil {
+		log.Error(err)
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 func GetUser(c *gin.Context) {
 	c.String(http.StatusNotImplemented, "Implement me!")
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,7 @@
 package services
+
+import "github.com/aushafy/go-bookstore-users-api/domain/users"
+
+func CreateUser(user users.User) (*users.User, error) {
+	return &user, nil
+}
